8871fbdd588dbac67854c23a06618083adc10644
siddhantsingh1997 <48853978+siddhantsingh1997@users.noreply.github.com>
2025-03-27 01:59:14 +0530

    removed quotes

--- src/app/components/aboutus/aboutus.js
+++ src/app/components/aboutus/aboutus.js
@@ -8,7 +8,7 @@ export default function Aboutus() {
           About us
         </h2>
         <div className="text-white  text-2xl text-center p-4">
-          "Founded in 2023, Nidhi Security Services is dedicated to providing
+          {`"Founded in 2023, Nidhi Security Services is dedicated to providing
           exceptional security solutions tailored to the unique needs of our
           clients through innovation and strong ethics. Led by a team with
           decades of experience in the industry, we ensure the highest standards
@@ -18,7 +18,7 @@ export default function Aboutus() {
           services. His guidance helps us combine innovation and reliability to
           deliver comprehensive protection. We are committed to being a trusted
           partner, safeguarding what matters most with a client-first approach
-          that prioritizes excellence, trust, and peace of mind. "
+          that prioritizes excellence, trust, and peace of mind. "`}
         </div>
       </div>
     </>
